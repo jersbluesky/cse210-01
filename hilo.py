39eafd218c7487986cc3a7db6b83a057f33b3570
jersbluesky <joh02052@byui.edu>
2022-05-07 13:06:34 -0600

    Psuedo-code

--- hilo.py
+++ hilo.py
@@ -2,3 +2,44 @@
 # CSE210 - Brother Duane Richards
 # Jeremy Johnson
 # Hilo
+
+
+# Player class 
+# - self.score eq 300 attribute
+# - player_score method 
+
+# Card class
+# - self.value eq 1
+# - card_value method
+
+# Game class
+# - self.next_card_value
+# - self.player_choice
+# - self.winner
+# - is_winner method
+# 
+# 
+# 
+
+# # Main function 
+# - create player 
+# - create card
+# - create game class
+# - SET play_again = y
+# - WHILE loop: 
+# - - WHILE play_again = y
+# - - - DISPLAY "The card is: {card.value}"
+# - - - PROMPT h or l from the user, SET to player.choice
+# - - - GET card.next_card_value (using random generator)
+# - - - IF game.is_winner eq true THEN player.score += 100
+# - - -  - ELSE player.score -= 100
+# - - - SET card.value eq card.new_card_value
+# - - - PROMPT ask user y or n, play again?
+# - - ELSE 
+# - - - DISPLAY "Your final score was: {player.score}"
+
+
+
+
+
+ 
